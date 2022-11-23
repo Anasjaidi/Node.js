028008c5535442003d7bdb7b937502d04ec9bbc7
Anas jaidi <anas.jaidi@icloud.com>
2022-11-24 00:13:02 +0100

    create simple server with basic router

--- learn-nodejs/web-server/dist/simpleServer.js
+++ learn-nodejs/web-server/dist/simpleServer.js
@@ -5,8 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const chalk_1 = __importDefault(require("chalk"));
+const log = console.log;
 const server = http_1.default.createServer((req, res) => {
-    res.end('wash a ferida');
+    if (req.url != "/favicon.ico")
+        log(req.url);
+    switch (req.url) {
+        case '/':
+            res.end("wash a ferida");
+            break;
+        default:
+            res.writeHead(404, {
+                'Content-type': 'text/html',
+            }); // ? always seted before sending the response 
+            res.end("<h1>page not found</h1>");
+            break;
+    }
 });
 server.listen(8000, 'localhost', () => {
     console.log(chalk_1.default.bold.green.bgGray('server start on http://localhost:8000 😩'));
--- learn-nodejs/web-server/src/simpleServer.ts
+++ learn-nodejs/web-server/src/simpleServer.ts
@@ -1,9 +1,21 @@
 import http from 'http'
 import  chalk from 'chalk'
-
+const log = console.log;
 
 const server = http.createServer((req, res) => {
-  res.end('wash a ferida')
+  if (req.url != "/favicon.ico") log(req.url);
+  switch (req.url) {
+    case '/':
+      res.end("wash a ferida");
+      break;
+      
+      default:
+        res.writeHead(404, {
+          'Content-type': 'text/html',
+        }) // ? always seted before sending the response 
+      res.end("<h1>page not found</h1>");
+      break;
+  }
 })
 
 
