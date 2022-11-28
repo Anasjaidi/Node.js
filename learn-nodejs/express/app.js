e4bc28132946ad1b95d074e9f0023c3b16e246d5
Anas jaidi <anas.jaidi@icloud.com>
2022-11-28 12:09:24 +0100

    finish all crud operations

--- learn-nodejs/express/app.js
+++ learn-nodejs/express/app.js
@@ -20,14 +20,13 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
-
 app.get('/api/v1/tours/:id', (req, res) => {
-  const tour = tours.find(el => el.id === req.params.id * 1)
+  const tour = tours.find((el) => el.id === req.params.id * 1)
 
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID 💥'
+      message: 'Invalid ID 💥',
     })
   }
 
@@ -39,13 +38,55 @@ app.get('/api/v1/tours/:id', (req, res) => {
   })
 })
 
-
 app.post('/api/v1/tours', async (req, res) => {
   const data = req.body
   data.id = tours[tours.length - 1].id + 1
-  fs.writeFile(path.join(__dirname, 'dev-data', 'tours.json'), JSON.stringify([...tours, data], null, 4), err => console.log(err))
+  fs.writeFile(
+    path.join(__dirname, 'dev-data', 'tours.json'),
+    JSON.stringify([...tours, data], null, 4),
+    (err) => console.log(err)
+  )
   res.send('hello')
 })
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find((el) => el.id === req.params.id * 1)
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID 💥',
+    })
+  }
+
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour: '<UPDATED TOUR>',
+    },
+  })
+})
+
+
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find((el) => el.id === req.params.id * 1)
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID 💥',
+    })
+  }
+
+  res.status(204).json({
+    status: 'succes',
+    data: {
+      tour: '<DELETED TOUR>',
+    },
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
