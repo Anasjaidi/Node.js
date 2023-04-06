3feffcd2597f6476892cf6724be70b920d6727d3
Anas jaidi <anas.jaidi@icloud.com>
2023-04-06 10:43:41 +0000

    finish users dao

--- prisma/classes/prismaUsersClient.js
+++ prisma/classes/prismaUsersClient.js
@@ -1,2 +1,20 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prismaClient = require('../prisma/client/prisma')
+
+
+
+class prismaUsersClient {
+
+  constructor(conf) {
+    this.prisma = prismaClient
+    this.users = this.prisma.user
+  }
+
+  async signup(user) {
+    return await this.users.create({user})
+  }
+}
+
+const prismaUsersClient = new prismaUsersClient()
+
+module.exports = prismaUsersClient
