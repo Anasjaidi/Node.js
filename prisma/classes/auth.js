ea562919ae9a131f2461c1ef64f1e94c49fcc404
anas jaidi <anas.jaidi@icloud.com>
2023-04-06 14:21:41 +0000

    finish signup

--- prisma/classes/auth.js
+++ prisma/classes/auth.js
@@ -2,26 +2,41 @@ const bcrypt = require("bcryptjs");
 const prismaUsersClient = require("../classes/prismaUsersClient");
 const jwt = require("jsonwebtoken");
 
-
 class Auth {
 	constructor() {}
 
 	async signup(body) {
+		const { firstName, lastName, email } = body;
 
-    const {firstName, lastName, email} = body
+		const password = await this.hashPassword(body.password, 12);
 
-    const password = await this.hashPassword(body.password, 12);
+		const newUser = await prismaUsersClient.addUser({
+			firstName,
+			lastName,
+			email,
+			password,
+		})
 
-    const newUser = await prismaUsersClient.addUser({firstName, lastName, email, password})
-  
-    return newUser
-  }
+    const token = this.generateToken(newUser.uid)
 
-  async hashPassword(pass, salt) {
-    return await bcrypt.hash(pass, salt)
-  }
+		return {newUser, token};
+	}
 
-  async checkPassword(candidatePassword, password) {
-    return await bcrypt.compare(candidatePassword, password)
-  }
+	async hashPassword(pass, salt) {
+		return await bcrypt.hash(pass, salt);
+	}
+
+	async checkPassword(candidatePassword, password) {
+		return await bcrypt.compare(candidatePassword, password);
+	}
+
+	generateToken(payload) {
+		return jwt.sign({ id: payload }, process.env.JWT_SECRET_KEY, {
+			expiresIn: process.env.JWT_EXPIRE_IN,
+		});
+	}
 }
+
+const authDAO = new Auth();
+
+module.exports = authDAO;
--- prisma/controllers/usersController.js
+++ prisma/controllers/usersController.js
@@ -1,14 +1,12 @@
+const authDAO = require('../classes/auth')
 const prismaUsersClient = require('../classes/prismaUsersClient')
 
 
 const signup = async (req, res, next) => {
-  const user = await prismaUsersClient.signup(req.body)
+  
+  const { newUser, token } = await authDAO.signup(req.body);
 
-  const token = await jwt.sign({ uid: user.uid }, process.env.JWT_SECRET_KEY, {
-		expiresIn: process.env.JWT_EXPIRES_IN,
-	});
-
-  res.status(201).json({status: 'success', data: user, token})
+  res.status(201).json({status: 'success', data: newUser, token})
 }
 
 const signin = async (req, res, next) =>  {
