a1c5bf6fff765088ec77db79f306983b665605d5
anas jaidi <anas.jaidi@icloud.com>
2023-04-15 01:23:05 +0000

    setup views and their handlers

--- passport/server.js
+++ passport/server.js
@@ -4,9 +4,16 @@ const ejs = require("ejs")
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log("yes");
-  res.status(200).json({})
+app.set("view-engine", "ejs")
+
+app.get('/',(req, res, next) => {
+  res.render('index.ejs')
+})
+app.get('/login',(req, res, next) => {
+  res.render('login.ejs')
+})
+app.get('/register',(req, res, next) => {
+  res.render('register.ejs')
 })
 
 app.listen(3000, () => {})
